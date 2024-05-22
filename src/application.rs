d91f0e28e44db8622baa7d08ea4affb8cfc0ef61
Wilhelm Carlsson <103310488+Mhiw@users.noreply.github.com>
2024-05-22 20:00:25 +0200

    Added the functionality to run functions

--- src/application.rs
+++ src/application.rs
@@ -2,13 +2,37 @@ use crate::renderer::Renderer;
 
 pub struct Application {
     renderer: Option<Renderer>,
+    running: bool,
+}
+
+pub enum RunOption {
+    Update,
+    Init,
 }
 
 impl Application {
     pub fn new() -> Application {
         Application {
             renderer: None,
+            running: false,
         }
+    }
+
+    pub fn execute(&self, function: fn(), option: RunOption) {
+        match option {
+            RunOption::Update => {
+                while self.running {
+                    function();
+                }
+            },
+            RunOption::Init => {
+                function();
+            },
+        };
+    }
+
+    pub fn quit(&mut self) {
+
     }
     
     pub fn set_renderer(&mut self, renderer: Renderer) {
